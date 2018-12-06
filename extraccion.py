04acd6b971e1bc057145e537f3714e9d449dc4cb
EliteArmy <ariel9501@yahoo.com>
2018-12-06 13:40:04 -0600

    Version 1.1

--- extraccion.py
+++ extraccion.py
@@ -67,15 +67,17 @@ for house in contenedor:
   datos = house.get_feats() # Consigue los datos de cada casa
 
   # Se desempaqueta el objeto de casas en diferentes listas:
-  price = datos['price']
-  location = datos['location']
-  size = datos['size']
-  num_bedrooms = datos['num_bedrooms']
-  num_bathrooms = datos['num_bathrooms']
+  price.append(datos['price'])
+  location.append(datos['location'])
+  size.append(datos['size'])
+  num_bedrooms.append(datos['num_bedrooms'])
+  num_bathrooms.append(datos['num_bathrooms'])
+  
   inner_feats = datos['inner_feats']
   outer_feats = datos['outer_feats']
   environ_feats = datos['environ_feats']
 
+  # Prueba de datos:
   #print("Datos:", datos)
   #print("Precio:", price)
   #print("Lugar:", location)
@@ -90,10 +92,15 @@ for house in contenedor:
   crearLista(inner_feats, len(inner_feats), outer_feats, len(outer_feats), environ_feats, len(environ_feats))
 
 print("------------------")
+print("Precio:", price)
+print("Lugar:", location)
+print("Tamaño:", size)
+print("Numero Habitaciones:", num_bedrooms)
+print("Numero de baños:", num_bathrooms)
 print("lista_inner_feats:", lista_inner_feats, len(lista_inner_feats))
 print("características Exteriores:", lista_outer_feats, len(lista_outer_feats))
 print("características Entorno:", lista_environ_feats, len(lista_environ_feats))
 
-  
+
 
 
