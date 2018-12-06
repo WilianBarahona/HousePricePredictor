bfa936c82445894056b82eb7f5f8a17f273f71cd
Apache_Killer <wilianbarahona10m@gmail.com>
2018-12-06 00:15:17 -0600

    housePricePredictor

---
+++ house_price_predictor.py
@@ -0,0 +1,50 @@
+class HousePricePredictor:
+
+  def __init__(self):
+    """El método init leerá todos los archivos con extensión ".html" de los directorios casas/train y casas/test y los usará para crear las matrices de X_train, X_test, Y_train y Y_test.
+
+    Cada archivo con formato "html" corresponderá a una instancia de entrenamiento o prueba. Deberá leer los atributos de cada casa tomando como base el código del proyecto anterior:
+
+    {
+      "price": "6370597",
+      "size": "267",
+      "num_bedrooms": "4",
+      "num_bathrooms": "3.5",
+      "location": "Colonia Alameda, Tegucigalpa, Francisco Morazán",
+      "inner_feats": {"cisterna", "comedor", "sistema de alarma"},
+      "outer_feats": {"garaje: si", "cuarto de servidumbre", "estudio", "sala"},
+      "environ_feats: {"area social"}
+    }
+
+    Sin embargo, antes de poder entrenar un modelo deberá convertir estos atributos a valores numéricos, los atributos size, num_bedrooms y num_bathrooms ya tienen valores numéricos, el atributo price también, pero este no se considerará como atributo, sino como la etiqueta de la instancia.
+
+    Para el resto de atributos, es decir: location, inner, outer y environ feats deberá utilizar la técnica conocida como One Hot Encoding (explicada en clase) para generar vectores binarios para cada atributo y unirlos al final en el vector de atributos definitivo. 
+
+    """
+    pass
+
+  def train_model(self):
+    """Retorna un un diccionario con los siguientes datos:
+    
+    d = {"costs": costs,
+    "w" : w, 
+    "b" : b,
+    "learning_rate" : learning_rate,
+    "num_iterations": num_iterations}
+    
+    costs, es una lista con los costos obtenidos durante el entrenamiento cada 100 interaciones.
+    learning_rate y num_iterations deben ser definidos por el programador mediante un proceso de prueba y error con el fin de obtener la mejor precisión en los datos de prueba.
+    """
+    pass
+
+  def get_datasets(self):
+    """Retorna un diccionario con los datasets preprocesados con los datos y 
+    dimensiones que se usaron para el entrenamiento
+    
+    d = { "X_train": X_train,
+    "X_test": X_test,
+    "Y_train": Y_train,
+    "Y_test": Y_test
+    }
+    """
+    pass
