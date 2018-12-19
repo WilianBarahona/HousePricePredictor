5feae9d892b1cdb9d7bcc9b74874fae6f8098d04
EliteArmy <ariel9501@yahoo.com>
2018-12-19 12:29:26 -0600

    error al identar

--- house_price_predictor.py
+++ house_price_predictor.py
@@ -1,4 +1,5 @@
 from extraccion_comentado import crearDataSets
+import numpy as np
 
 class HousePricePredictor:
 
@@ -62,36 +63,36 @@ class HousePricePredictor:
 
     ## --- 2. Initialize the model's parameters --- ###
     def initialize_parameters(n_x, n_h, n_y):
-    """
-    **Argument:
-    n_x -- size of the input layer
-    n_h -- size of the hidden layer
-    n_y -- size of the output layer
-    **Returns:
-    params -- python dictionary containing your parameters:
-                    W1 -- weight matrix of shape (n_h, n_x)
-                    b1 -- bias vector of shape (n_h, 1)
-                    W2 -- weight matrix of shape (n_y, n_h)
-                    b2 -- bias vector of shape (n_y, 1)
-    """
-    np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
-    
-    W1 = np.random.randn(n_h, n_x) * 0.01 
-    b1 = np.zeros((n_h, 1)) 
-    W2 = np.random.randn(n_y, n_h) * 0.01
-    b2 = np.zeros((n_y, 1))
-    
-    assert (W1.shape == (n_h, n_x))
-    assert (b1.shape == (n_h, 1))
-    assert (W2.shape == (n_y, n_h))
-    assert (b2.shape == (n_y, 1))
-    
-    parameters = {"W1": W1,
-                  "b1": b1,
-                  "W2": W2,
-                  "b2": b2}
-    
-    return parameters
+      """
+      **Argument:
+      n_x -- size of the input layer
+      n_h -- size of the hidden layer
+      n_y -- size of the output layer
+      **Returns:
+      params -- python dictionary containing your parameters:
+                      W1 -- weight matrix of shape (n_h, n_x)
+                      b1 -- bias vector of shape (n_h, 1)
+                      W2 -- weight matrix of shape (n_y, n_h)
+                      b2 -- bias vector of shape (n_y, 1)
+      """
+      np.random.seed(2) # we set up a seed so that your output matches ours although the initialization is random.
+      
+      W1 = np.random.randn(n_h, n_x) * 0.01 
+      b1 = np.zeros((n_h, 1)) 
+      W2 = np.random.randn(n_y, n_h) * 0.01
+      b2 = np.zeros((n_y, 1))
+      
+      assert (W1.shape == (n_h, n_x))
+      assert (b1.shape == (n_h, 1))
+      assert (W2.shape == (n_y, n_h))
+      assert (b2.shape == (n_y, 1))
+      
+      parameters = {"W1": W1,
+                    "b1": b1,
+                    "W2": W2,
+                    "b2": b2}
+      
+      return parameters
 
     ### --- ¡¡ Inicio !! --- ###
     X = self._X_train
@@ -103,7 +104,7 @@ class HousePricePredictor:
     #print("The size of the output layer is: n_y = " + str(n_y))
 
     learning_rate = 0.01 # Hay que ver que tan alto o bajo será el learning rate
-    num_iterations = 80000 # Habra que ver cuantas iteraciones es un numero aceptable
+    numero_iteraciones = 80000 # Habra que ver cuantas iteraciones es un numero aceptable
 
     parameters = initialize_parameters(n_x, n_h, n_y) # Llama a la función 2.
     costs = []
